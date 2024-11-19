56b6f1ccfca899a57c4fb69ee1dff482690c5eda
Marcin8811 <150076087+Marcin8811@users.noreply.github.com>
2024-11-19 19:38:49 +0100

    http_methods.py

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,27 +1,40 @@
+import allure
 import requests
+from utils.logger import Logger
 
-"""Lista HTTP methods"""
+"""Lista HTTP-metod"""
 class Http_methods:
     headers = {'Content-Type': 'application/json'}
     cookie = ""
+
     @staticmethod
     def get(url):
-        result = requests.get(url, headers=Http_methods.headers, cookies=Http_methods.cookie)
-        return result
+        with allure.step("GET"):
+            Logger.add_request(url, method="GET")
+            result = requests.get(url, headers=Http_methods.headers, cookies=Http_methods.cookie)
+            Logger.add_response(result)
+            return result
 
     @staticmethod
     def post(url, body):
-        result = requests.post(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
-        return result
-
+        with allure.step("POST"):
+            Logger.add_request(url, method="POST")
+            result = requests.post(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+            Logger.add_response(result)
+            return result
 
     @staticmethod
     def put(url, body):
-        result = requests.put(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
-        return result
-
+        with allure.step("PUT"):
+            Logger.add_request(url, method="PUT")
+            result = requests.put(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+            Logger.add_response(result)
+            return result
 
     @staticmethod
     def delete(url, body):
-        result = requests.delete(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
-        return result
+        with allure.step("DELETE"):
+            Logger.add_request(url, method="DELETE")
+            result = requests.delete(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+            Logger.add_response(result)
+            return result
