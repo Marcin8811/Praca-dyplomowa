0f76ab3523aa3fd370822b9baaf1748c5ac1701a
Marcin8811 <150076087+Marcin8811@users.noreply.github.com>
2024-12-08 17:21:46 +0100

    logger.py 08.12.2024

--- utils/logger.py
+++ utils/logger.py
@@ -1,16 +1,21 @@
 import datetime
 import os
+import logging
 from requests import Response
 
+# Ustawenia logirowania
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-class Logger():
-    file_name = f"logs/log_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + " .log"
+class Logger:
+    file_name = f"logs/log_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".log"
+
     @classmethod
     def write_log_to_file(cls, data: str):
-        #  Sprzawdzamy, czy jest folder dla logów, jeżeli nie ma, to robimy
+        # Sprawdzamy, czy jest folder dla logów, jeżeli nie ma, to robimy
         os.makedirs(os.path.dirname(cls.file_name), exist_ok=True)
         with open(cls.file_name, 'a', encoding='utf-8') as logger_file:
             logger_file.write(data)
+        logging.info(f"Zapisano logi do pliku: {cls.file_name}")
 
     @classmethod
     def add_request(cls, url: str, method: str):
@@ -18,23 +23,24 @@ class Logger():
 
         date_to_add = f"\n-----\n"
         date_to_add += f"Test: {test_name}\n"
-        date_to_add += f"Time: {str(datetime.datetime.now())}\n"
-        date_to_add += f"Request method: {method}\n"
-        date_to_add += f"Request URL: {url}\n"
+        date_to_add += f"Czas: {str(datetime.datetime.now())}\n"
+        date_to_add += f"Metoda zapytania: {method}\n"
+        date_to_add += f"URL zapytania: {url}\n"
         date_to_add += "\n"
 
         cls.write_log_to_file(date_to_add)
+        logging.info(f"Zarejestrowano zapytanie: {method} {url}")
 
     @classmethod
     def add_response(cls, result: Response):
         cookies_as_dict = dict(result.cookies)
         headers_as_dict = dict(result.headers)
 
-        date_to_add = f"Response code: {result.status_code}\n"
-        date_to_add += f"Response text: {result.text}\n"
-        date_to_add += f"Response headers: {headers_as_dict}\n"
-        date_to_add += f"Response cookies: {cookies_as_dict}\n"
+        date_to_add = f"Status odpowiedzi: {result.status_code}\n"
+        date_to_add += f"Treść odpowiedzi: {result.text}\n"
+        date_to_add += f"Nagłówki odpowiedzi: {headers_as_dict}\n"
+        date_to_add += f"Ciasteczka odpowiedzi: {cookies_as_dict}\n"
         date_to_add += f"\n-----\n"
 
         cls.write_log_to_file(date_to_add)
-
+        logging.info(f"Odpowiedź zapisana: Status: {result.status_code}, Treść: {result.text[:100]}...")  # Przykład skrócenia treści odpowiedzi
