3b88d72b28e803569607cb74d9306ad74bc827cb
Marcin8811 <150076087+Marcin8811@users.noreply.github.com>
2024-12-08 17:19:49 +0100

    test_google_maps_api.py 08.12.2024

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,71 +1,102 @@
-import json
+import logging
 import allure
 from requests import Response
-from utils.checking import Checking
+from utils.checker import Checker
 from utils.api import Google_maps_api
 
-"""Tworzenie, zmiana i usunięcie nowej lokalizacji"""
-@allure.epic("Test create place")
-class TestCreatePlace:
-    @allure.description("Test create, update, delete new place")
+# Ustawienia logowania
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+@allure.epic("Test Google Maps API")
+class TestGoogleMapsAPI:
+
+    @allure.description("Test tworzenia nowego miejsca")
     def test_create_new_place(self):
-        print("Method POST") # Robimy nowe miejsce
+        logging.info("Metoda POST: Tworzenie nowego miejsca")
         result_post = Google_maps_api.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
-        Checking.check_status_code(result_post, 200)
-        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
-        token = json.loads(result_post.text)
-        Checking.check_json_value(result_post, 'status', 'OK')
 
-        print("Method GET POST")    # Otrzymujemu info o tworzeniu nowego miejsca
+        # Sprawdzamy kod statusu i inne parametry
+        Checker.check_status_code(result_post, 200)
+        Checker.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        Checker.check_json_value(result_post, 'status', 'OK')
+
+        # Zwracamy place_id do wykorzystania w kolejnych testach
+        assert place_id is not None, "place_id nie powinno być None"
+        logging.info(f"Utworzono nowe miejsce z place_id: {place_id}")
+        return place_id
+
+    @allure.description("Test pobierania nowego miejsca")
+    def test_get_new_place(self):
+        logging.info("Metoda GET: Pobieranie informacji o nowym miejscu")
+        place_id = "some_existing_place_id"  # Zastąp prawdziwym place_id
         result_get: Response = Google_maps_api.get_new_place(place_id)
-        if result_get is not None:
-            Checking.check_status_code(result_get, 200)
-        else:
-            print("Błąd: result_get wrócił None")
-            assert False, "API wrócił None"
-        Checking.check_status_code(result_get, 200)
-        Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
-        Checking.check_json_value(result_get, 'address', '29, side layout,cohen 09')
 
-        print("Method PUT")         # Aktualizujemy info o miejscu
+        # Sprawdzamy kod statusu i obecność wymaganych pól
+        Checker.check_status_code(result_get, 200)
+        Checker.check_json_token(result_get,
+                                 ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
+                                  'language'])
+        Checker.check_json_value(result_get, 'address',
+                                 '29, side layout,cohen 09')  # Zakładając, że to jest początkowy adres
+
+    @allure.description("Test aktualizacji miejsca")
+    def test_update_place(self):
+        logging.info("Metoda PUT: Aktualizacja informacji o miejscu")
+        place_id = "some_existing_place_id"  # Zastąp prawdziwym place_id
         result_put: Response = Google_maps_api.put_new_place(place_id)
-        Checking.check_status_code(result_put, 200)
-        Checking.check_json_token(result_put, ['msg'])
-        Checking.check_json_value(result_put, 'msg', 'Address successfully updated')
 
-        print("Method GET PUT")     # Otrzymujemy aktualizowaną info o miejscu
-        result_get: Response = Google_maps_api.get_new_place(place_id)
-        Checking.check_status_code(result_get, 200)
-        Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
-        Checking.check_json_value(result_get, 'address', '100 Lenina street, RU')
+        # Sprawdzamy kod statusu i komunikat o pomyślnej aktualizacji
+        Checker.check_status_code(result_put, 200)
+        Checker.check_json_token(result_put, ['msg'])
+        Checker.check_json_value(result_put, 'msg', 'Address successfully updated')
 
-        print("Method DELETE")      # Usuwamy miejsce
+    @allure.description("Test usuwania miejsca")
+    def test_delete_place(self):
+        logging.info("Metoda DELETE: Usuwanie miejsca")
+        place_id = "some_existing_place_id"  # Zastąp prawdziwym place_id
         result_delete: Response = Google_maps_api.delete_new_place(place_id)
-        Checking.check_status_code(result_delete, 200)
-        Checking.check_json_token(result_delete, ['status'])
-        Checking.check_json_value(result_delete, 'status', 'OK')
 
-        print("Method GET DELETE")     # Sprawdzamy, że miejsce było usuniętę
+        # Sprawdzamy kod statusu i komunikat o pomyślnym usunięciu
+        Checker.check_status_code(result_delete, 200)
+        Checker.check_json_token(result_delete, ['status'])
+        Checker.check_json_value(result_delete, 'status', 'OK')
+
+        # Sprawdzamy, czy miejsce zostało usunięte
         result_get: Response = Google_maps_api.get_new_place(place_id)
+        assert result_get.status_code == 404, f"Oczekiwano 404, ale otrzymano {result_get.status_code}"
+        logging.info(f"Miejsce z place_id {place_id} zostało pomyślnie usunięte")
 
-        # Sprawdzamy, że po usunięciu my otrzymujemy 404 i dobry msg
-        if result_get.status_code == 404:
-            print("Miesjce usunięto dobrze, otrzymali status 404.")
-            print('msg: Get operation failed, looks like place_id doesn\'t exists')
-        else:
-            print("Błąd: Miejsce nie było usunięte albo wracamy nieczakany status")
-            assert False, f"Expected 404, but got {result_get.status_code}"
+    # Negatywne testy
 
-        # Logika do usunięcia (jeżeli lokalizacja nie istnieje, można wydrukować dodatkowy komunikat)
-        if result_delete.status_code == 200:
-            print("Usunięcie powiodło się")
-        else:
-            print("Błąd usuwania, najprawdopodobniej lokalizacja nie istnieje")
-            print('msg: Delete operation failed, looks like the data doesn\'t exists')
-        Checking.check_json_token(result_put, ['msg'])
-        Checking.check_json_search_word_in_value(result_get, 'msg', 'failed')
+    @allure.description("Test negatywny: Tworzenie miejsca z brakującymi wymaganymi polami")
+    def test_create_new_place_negative(self):
+        logging.info("Metoda POST: Próba utworzenia nowego miejsca z brakującymi polami")
+        invalid_place_data = {
+            "location": {"lat": -38.383494, "lng": 33.427362}
+            # Pozostawiamy ciało żądania niekompletne
+        }
+        result_post = Google_maps_api.create_new_place(invalid_place_data)
+        Checker.check_status_code(result_post, 400)  # Oczekujemy błędu 400
 
+    @allure.description("Test negatywny: Pobieranie miejsca z nieprawidłowym place_id")
+    def test_get_new_place_negative(self):
+        logging.info("Metoda GET: Próba pobrania miejsca z nieprawidłowym place_id")
+        invalid_place_id = "invalid_place_id"
+        result_get = Google_maps_api.get_new_place(invalid_place_id)
+        Checker.check_status_code(result_get, 404)  # Oczekujemy błędu 404
 
-        print("Testowanie tworzenij, zmian i usunięc nowej lokalizacji skończyło się dobrze!")
+    @allure.description("Test negatywny: Aktualizacja miejsca z nieprawidłowym place_id")
+    def test_update_place_negative(self):
+        logging.info("Metoda PUT: Próba aktualizacji miejsca z nieprawidłowym place_id")
+        invalid_place_id = "invalid_place_id"
+        result_put = Google_maps_api.put_new_place(invalid_place_id)
+        Checker.check_status_code(result_put, 404)  # Oczekujemy błędu 404
+
+    @allure.description("Test negatywny: Usuwanie miejsca z nieprawidłowym place_id")
+    def test_delete_place_negative(self):
+        logging.info("Metoda DELETE: Próba usunięcia miejsca z nieprawidłowym place_id")
+        invalid_place_id = "invalid_place_id"
+        result_delete = Google_maps_api.delete_new_place(invalid_place_id)
+        Checker.check_status_code(result_delete, 404)  # Oczekujemy błędu 404
