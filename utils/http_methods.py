5964b03f5625b0240f8987722aafeca05fb92bbb
Marcin8811 <150076087+Marcin8811@users.noreply.github.com>
2024-12-08 17:22:49 +0100

    http_methods.py 08.12.2024

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,40 +1,42 @@
-import allure
+import logging
 import requests
 from utils.logger import Logger
 
-"""Lista HTTP-metod"""
+# Ustawenia logirowania
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 class Http_methods:
     headers = {'Content-Type': 'application/json'}
     cookie = ""
 
     @staticmethod
     def get(url):
-        with allure.step("GET"):
-            Logger.add_request(url, method="GET")
-            result = requests.get(url, headers=Http_methods.headers, cookies=Http_methods.cookie)
-            Logger.add_response(result)
-            return result
+        logging.info(f"Zapytanie GET: {url}")
+        result = requests.get(url, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        logging.info(f"Odpowiedź od GET: {result.status_code} - {result.text}")
+        Logger.add_response(result)
+        return result
 
     @staticmethod
     def post(url, body):
-        with allure.step("POST"):
-            Logger.add_request(url, method="POST")
-            result = requests.post(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
-            Logger.add_response(result)
-            return result
+        logging.info(f"Zapytanie POST: {url}")
+        result = requests.post(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        logging.info(f"Odpowiedź od POST: {result.status_code} - {result.text}")
+        Logger.add_response(result)
+        return result
 
     @staticmethod
     def put(url, body):
-        with allure.step("PUT"):
-            Logger.add_request(url, method="PUT")
-            result = requests.put(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
-            Logger.add_response(result)
-            return result
+        logging.info(f"Zapytanie PUT: {url}")
+        result = requests.put(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        logging.info(f"Odpowiedź od PUT: {result.status_code} - {result.text}")
+        Logger.add_response(result)
+        return result
 
     @staticmethod
     def delete(url, body):
-        with allure.step("DELETE"):
-            Logger.add_request(url, method="DELETE")
-            result = requests.delete(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
-            Logger.add_response(result)
-            return result
+        logging.info(f"Zapytanie DELETE: {url}")
+        result = requests.delete(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        logging.info(f"Odpowiedź od DELETE: {result.status_code} - {result.text}")
+        Logger.add_response(result)
+        return result
